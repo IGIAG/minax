79b3c0191b8eb8a72456a20fdb12966f408baded
Krzysiek <63916404+IGIAG@users.noreply.github.com>
2024-12-03 20:38:18 +0100

    This is better

--- source/smart.d
+++ source/smart.d
@@ -7,47 +7,42 @@ import block_matrix;
 import core.bitop : popcnt;
 
 import std.stdio;
+import std.string;
 
 import std.random;
 
+struct Record {
+    SimpleImplicant implicant;
+    uint covered;
+}
+
 SimpleImplicant[] smart_method(uint[] F, uint[] R, char[] column_names)
 {
-    if (F.length == 0)
+    SimpleImplicant[] returnable;
+    SimpleImplicant[uint] coverage_map;
+    foreach (uint cube; F)
     {
+        SimpleImplicant[] implicants = get_simple_implicant(cube, generate_block_matrix(cube, R), 2 << column_names.length, column_names);
+        foreach (SimpleImplicant implicant; implicants)
+        {
+            uint coverage = cast(uint)(F.length - remove_values_matching_simple_implicant(F.dup,implicant).length);
+            coverage_map[coverage] = implicant;
+        }
+    }
+    SimpleImplicant best;
+    uint best_coverage = 0;
+
+    foreach (key; coverage_map.keys)
+    {
+        if(key > best_coverage){
+            best = coverage_map[key];
+            best_coverage = key;
+        }
+    }
+    writeln(coverage_map);
+    if(F.length == 0){
         return [];
     }
-    writeln("step");
-    foreach (uint row; F)
-    {
-        writefln("Remaining: %b", row);
-    }
-    writeln("--");
-    SimpleImplicant[] returnable = [];
-    uint[] zero_matrix = generate_block_matrix(F[0], R);
-    SimpleImplicant best = get_simple_implicant(F[0], zero_matrix, cast(int)(
-            column_names.length * column_names.length), column_names)[0];
-    uint best_coverage = uint.max;
-    foreach (uint k; F)
-    {
-
-        uint[] block_matrix = generate_block_matrix(k, R);
-        SimpleImplicant[] new_implicants = get_simple_implicant(k, block_matrix, cast(int)(
-                column_names.length * column_names.length), column_names);
-        if(new_implicants.length == 0){
-            continue;
-        }
-        SimpleImplicant implicant = new_implicants[0];
-        uint coverage = cast(uint) remove_values_matching_simple_implicant(F.dup, implicant).length;
-        if (coverage < best_coverage)
-        {
-            best = implicant;
-            best_coverage = coverage;
-
-        }
-    }
-        returnable ~= best;
-
-        return returnable ~ smart_method(remove_values_matching_simple_implicant(F.dup, best), R, column_names);
+    return smart_method(remove_values_matching_simple_implicant(F.dup,best),R,column_names) ~ best;
 
 }
-    
