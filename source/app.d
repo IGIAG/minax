8378623eeea4e3a97fffbc3b340cceb5a3858ee3
Krzysiek <63916404+IGIAG@users.noreply.github.com>
2024-12-03 15:23:57 +0100

    I hate ts

--- source/app.d
+++ source/app.d
@@ -5,6 +5,8 @@ import block_matrix;
 import input_output;
 import heuristic;
 
+import smart;
+
 void main(string[] args)
 {
 	writeln(import("intro.txt"));
@@ -20,12 +22,20 @@ void main(string[] args)
 		writefln("Odczytywanie z pliku: %s\n",args[1]);
 		input_output.read_from_file(args[1], column_names, F, R);
 	}
+	SimpleImplicant[] smart_simple_implicants = smart.smart_method(F.dup,R.dup,column_names.dup);
 	SimpleImplicant[] simple_implicants = heuristic.heuristic_method(F,R,column_names);
+	
 	
 	string[] stringed_simple_implicants = [];
 	foreach (SimpleImplicant simple_implicant; simple_implicants)
 	{
 		stringed_simple_implicants ~= simple_implicant_to_string(simple_implicant, column_names);
 	}
-	writefln("Uproszczone wyrazenie booleowskie: %s", stringed_simple_implicants.join(" + "));
+	string[] stringed_smart_simple_implicants = [];
+	foreach (SimpleImplicant simple_implicant; smart_simple_implicants)
+	{
+		stringed_smart_simple_implicants ~= simple_implicant_to_string(simple_implicant, column_names);
+	}
+	writefln("Uproszczone wyrazenie booleowskie (h): %s", stringed_simple_implicants.join(" + "));
+	writefln("Uproszczone wyrazenie booleowskie (s): %s", stringed_smart_simple_implicants.join(" + "));
 }
---
+++ source/smart.d
@@ -0,0 +1,51 @@
+module smart;
+
+import simple_implicant;
+
+import block_matrix;
+
+import core.bitop : popcnt;
+
+import std.stdio;
+
+SimpleImplicant[] smart_method(uint[] F, uint[] R, char[] column_names)
+{
+    if (F.length == 0)
+    {
+        return [];
+    }
+    writeln("step");
+    foreach (uint row; F)
+    {
+        writefln("Remaining: %b", row);
+    }
+    writeln("--");
+    SimpleImplicant[] returnable = [];
+    uint[] zero_matrix = generate_block_matrix(F[0], R);
+    SimpleImplicant best = get_simple_implicant(F[0], zero_matrix, cast(int)(
+            column_names.length * column_names.length), column_names)[0];
+    uint best_coverage = 0;
+    foreach (uint k; F)
+    {
+
+        uint[] block_matrix = generate_block_matrix(k, R);
+        SimpleImplicant[] new_implicants = get_simple_implicant(k, block_matrix, cast(int)(
+                column_names.length * column_names.length), column_names);
+        if(new_implicants.length == 0){
+            continue;
+        }
+        SimpleImplicant implicant = new_implicants[0];
+        uint coverage = cast(uint) remove_values_matching_simple_implicant(F.dup, implicant).length;
+        if (coverage > best_coverage)
+        {
+            best = implicant;
+            best_coverage = coverage;
+
+        }
+    }
+        returnable ~= best;
+
+        return returnable ~ smart_method(remove_values_matching_simple_implicant(F.dup, best), R, column_names);
+
+}
+    
