066ad652cc6b98c47bf1ca73ab2fc1ac2a164eb8
igiag <me@igiag.net>
2024-12-04 13:35:36 +0100

    temporary removed the other method

--- source/app.d
+++ source/app.d
@@ -22,7 +22,7 @@ void main(string[] args)
 		writefln("Odczytywanie z pliku: %s\n",args[1]);
 		input_output.read_from_file(args[1], column_names, F, R);
 	}
-	SimpleImplicant[] smart_simple_implicants = smart.smart_method(F.dup,R.dup,column_names.dup);
+	//SimpleImplicant[] smart_simple_implicants = smart.smart_method(F.dup,R.dup,column_names.dup);
 	SimpleImplicant[] simple_implicants = heuristic.heuristic_method(F,R,column_names);
 	
 	
@@ -32,10 +32,10 @@ void main(string[] args)
 		stringed_simple_implicants ~= simple_implicant_to_string(simple_implicant, column_names);
 	}
 	string[] stringed_smart_simple_implicants = [];
-	foreach (SimpleImplicant simple_implicant; smart_simple_implicants)
+	/*foreach (SimpleImplicant simple_implicant; smart_simple_implicants)
 	{
 		stringed_smart_simple_implicants ~= simple_implicant_to_string(simple_implicant, column_names);
-	}
+	}*/
 	writefln("Uproszczone wyrazenie booleowskie (h): %s", stringed_simple_implicants.join(" + "));
-	writefln("Uproszczone wyrazenie booleowskie (s): %s", stringed_smart_simple_implicants.join(" + "));
+	//writefln("Uproszczone wyrazenie booleowskie (s): %s", stringed_smart_simple_implicants.join(" + "));
 }
